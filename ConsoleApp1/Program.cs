3e50d53bd6e8525e129cd086f25ae95ac15fc901
Tesl1k <danildavydov161@mail.ru>
2022-02-10 08:52:31 +0300

    LR01

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DucksLyb;
 
 namespace ConsoleApp1
 {
@@ -10,7 +11,20 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            MallardDuck mallardDuck = new MallardDuck();
+            RedheadDuck redheadDuck = new RedheadDuck();
 
+            Duck[] ducks = new Duck[] { mallardDuck, redheadDuck };
+
+            for ( int i = 0; i < ducks.Length; i++ )
+            {
+                Console.WriteLine(ducks[i].Quack());
+                Console.WriteLine(ducks[i].Swim());
+                Console.WriteLine(ducks[i].Display());
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
         }
     }
 }
---
+++ DucksLyb/Duck.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DucksLyb
+{
+    abstract public class Duck
+    {
+        public string Quack()
+        {
+            return "Quack" + " " + this.GetType();
+        }
+
+        public string Swim()
+        {
+            return "Swim" + " " + this.GetType();
+        }
+        abstract public string Display();
+    }
+}
